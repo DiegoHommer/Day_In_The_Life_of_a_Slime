6c3ad579af3f2e5f6ffa56e9aa8162f1ebea2e05
lucas-49 <lucasdepaulafonseca@gmail.com>
2024-03-24 00:03:20 -0300

    Update pc.gd

--- pc.gd
+++ pc.gd
@@ -9,7 +9,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
+	#aaaaa
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
