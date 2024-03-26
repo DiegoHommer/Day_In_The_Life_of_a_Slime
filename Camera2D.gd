b8046dcac5e0ad1fb7a4bd3d2a0865029db89586
lucas-49 <lucasdepaulafonseca@gmail.com>
2024-03-26 01:05:16 -0300

    Velocidade, e mais dos filhos
    Fiz ele aumentar menos por lixo (talvez tenha ficado pouco). A velocidade tá diminuindo de acordo com o tamanho.
    
    Fiz com que a cada 5 lixos ele possa fazer um filho.
    
    Ajeitei os problemas que tinha com os filhos seguindo dependendo do tamanho.

--- Camera2D.gd
+++ Camera2D.gd
@@ -2,14 +2,17 @@ extends Camera2D
 
 var direction = Vector2(0,0)
 
-#Como o pc tá se mexeno com o move_and_slide() que é pronto do Godot, a velocidade dele tem que ser mais 
+#Pra fazer ele ficar mais rápido quando o player sai do centro, eu não tô normalizando a posição
+#Além disso, a velocidade do pc tem que ser maior do que a velocidade "real" dele pelo jeito como
+#o move_and_slide() funciona
+#por isso a velocidade da câmera tem que ser a do pc vezes um multiplicador bem pequeno
 var speed_multiplier = 0.00002
 @onready var pc = %PC
 var speed = 0
 var velocity = Vector2(0,0)
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	speed = pc.speed*0.00002
+	speed = pc.speed*speed_multiplier
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scenes/filho.gd
+++ scenes/filho.gd
@@ -3,9 +3,11 @@ extends CharacterBody2D
 var parent = ""
 var pc = ""
 var speed = 250
-var numero = 0 #esse numero vai ser setado pelo pc quando ele criar o filho. É 1 se é o primeiro, 2 se é o seugundo etc
+var numero = 0 #esse numero é setado pelo pc quando ele cria o filho. Diz se ele é o primeiro, segundo, etc
 var move = true
 var direction = Vector2(0,0)
+var escala = 1
+var mult_dist = 1
 
 func _ready():
 	parent = get_parent() #"parent" aqui é o parent node (que é o main por enquanto) e não o pc
@@ -17,10 +19,13 @@ func _physics_process(delta):
 	
 	if move and pc.direction != Vector2(0,0): #
 		
-		#a ideia é que ele segue um ponto que tá um pouco atrás do filho anterior
+		#isso serve pra ajustar o ponto que ele segue dependendo do tamanho do pc 
+		mult_dist = parent.get_child(numero-1).escala 
+		
+		#a ideia é que ele segue um ponto que tá um pouco atrás do filho anterior (ou pc, se for o primeiro filho)
 		#(aqui, parent.get_child(numero-1) é o filho anterior)
 		#eu tentei fazer ele seguir diretamente o filho anterior, mas daí eles se amontoavam
-		direction = (parent.get_child(numero-1).position-30*parent.get_child(numero-1).direction) - position
+		direction = (parent.get_child(numero-1).position-30*mult_dist*parent.get_child(numero-1).direction) - position
 		direction = direction.normalized()
 		velocity = speed*direction
 		move_and_slide()
